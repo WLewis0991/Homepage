df6a9bac0b8a248c7582af496c87f31f9f6b66d0
wlewis0991 <wlewis1990@gmail.com>
2026-03-06 09:00:08 -0800

    Links added, dark and light moded added and calibrated. Grid bug fixed, good to go!

--- src/index.js
+++ src/index.js
@@ -2,3 +2,43 @@ import './styles.css';
 
 
 // Main JS index
+
+const toggle = document.getElementById("dayNightToggle");
+
+toggle.addEventListener("click", () => {
+    const body = document.querySelector("body");
+    const gitDark = document.querySelectorAll(".githubLink")
+    const gitLight = document.querySelectorAll(".githubLinkLight")
+    const linkDark = document.querySelectorAll(".demoLink");
+    const linkLight = document.querySelectorAll(".demoLinkLight")
+    if(body.classList.contains("lightmode")){
+        body.classList.remove("lightmode");
+        gitLight.forEach(git => {
+            git.classList.add("hidden")
+        });
+        gitDark.forEach(git => {
+            git.classList.remove("hidden")
+        })
+        linkLight.forEach(link => {
+            link.classList.add("hidden")
+        });
+        linkDark.forEach(link => {
+            link.classList.remove("hidden")
+        })
+
+    }else{
+        body.classList.add("lightmode");
+        gitLight.forEach(git => {
+            git.classList.remove("hidden")
+        });
+        gitDark.forEach(git => {
+            git.classList.add("hidden")
+        })
+        linkLight.forEach(link => {
+            link.classList.remove("hidden")
+        });
+        linkDark.forEach(link => {
+            link.classList.add("hidden")
+        });
+    }
+})
